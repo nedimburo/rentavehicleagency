a3ab132e4277590f3af7915dfc42c6854ea77b4f
nedimburo <nedimb9402@gmail.com>
2024-07-25 18:30:35 +0200

    Remowing frontend files from the backend

--- src/main/resources/static/js/imageForm.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-	$("#showFormButton").click(function (){
-		$("#profileImageForm").show();
-	});
-	$("#hideFormButton").click(function (){
-		$("#profileImageForm").hide();
-	});
-});
--- src/main/resources/static/js/sidemenu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-	$("#menuButton, #closeButton").click(function () {
-		$(".side-menu").animate({left: '0'}, 300);
-	});
-    $(".side-menu a, #closeButton").click(function () {
-        $(".side-menu").animate({left: '-250px'}, 300);
-	});
-});
